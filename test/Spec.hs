f44d0f2842aef58869328e5abef59ba54911fd3b
Takayuki Muranushi <muranushi@gmail.com>
2015-12-06 14:57:56 +0900

    test z3 on travis

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,15 +1,20 @@
 import Test.Framework (defaultMain, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 
+import Control.Monad.IO.Class
+import Data.SBV
+
 import Test.QuickCheck
+import Test.QuickCheck.Monadic
 
 main :: IO ()
 main = defaultMain tests
 
 tests = [
         testGroup "Sorting Group 1" [
-                testProperty "prop1" prop1,
-                testProperty "prop2" prop2
+                testProperty "logic is correct" prop1,
+                testProperty "the answer is not reachable by QuickCheck" prop2,
+                testProperty "Z3 can find the answer" prop3
            ]
       ]
 
@@ -18,3 +23,9 @@ prop1 b = b || True
 
 prop2 i = i /= 42
   where types = (i :: Int)
+
+prop3 :: Property
+prop3 = monadicIO $ do
+  result <- liftIO $ prove $ \x y -> x*y .== (y*x::SInteger)
+  -- in case of proof, assert non-existence of counterexample
+  assert $ not $ modelExists result
